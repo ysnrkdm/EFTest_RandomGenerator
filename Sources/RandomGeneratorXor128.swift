1a194f23b0ca3e309cf993650a27bc67f6d94148
ysnrkdm <euphonictech@gmail.com>
2016-10-24 20:10:28 +0900

    Support seed

--- Sources/RandomGeneratorXor128.swift
+++ Sources/RandomGeneratorXor128.swift
@@ -7,8 +7,9 @@
 //
 
 public class RandomGeneratorXor128 {
-    open static var generator = {
-        return RandomGeneratorXor128(seed: 19337)
+    open static var seed: Int = 19937
+    open static var generator: () -> RandomGeneratorXor128 = {
+        return RandomGeneratorXor128(seed: RandomGeneratorXor128.seed)
     }
     
     var seed: UInt64
@@ -19,15 +20,16 @@ public class RandomGeneratorXor128 {
     var w: UInt64
         
     fileprivate init(seed: Int) {
-        self.seed = UInt64.init(seed)
+        self.seed = UInt64.init(abs(seed))
         self.x = 123456789
         self.y = 362436069
         self.z = 521288629
         self.w = 88675123
-        
-        self.z ^= self.seed;
-        self.z ^= z >> 21; z ^= z << 35; z ^= z >> 4;
+//        print("seed used: \(seed)")
+        self.z ^= self.seed
+        self.z ^= z >> 21; z ^= z << 35; z ^= z >> 4
         self.z = self.z &* 2685821657736338717
+//        print("and z is initialized to: \(z)")
     }
     
     func xor128() -> UInt64 {
